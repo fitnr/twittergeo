ac143d41b72b096dc910da90e05787204042b873
fitnr <fitnr@fakeisthenewreal>
2015-07-27 16:59:39 -0400

    don't use TBU for arguments

--- twittergeo/cli.py
+++ twittergeo/cli.py
@@ -11,12 +11,15 @@
 from . import twittergeo
 import argparse
 import json
-from twitter_bot_utils import api, args
+from twitter_bot_utils.api import API
 from tweepy import Cursor
 
 def main():
     parser = argparse.ArgumentParser('twittergeo', description='Pull Twitter searches into GeoJSON')
-    args.add_no_user_args(parser)
+
+    parser.add_argument('-c', '--config', metavar='path', default=None, type=str, help='path to config file to parse (json or yaml)')
+    parser.add_argument('--consumer-key', type=str, metavar='key', help='Twitter consumer key')
+    parser.add_argument('--consumer-secret', type=str, metavar='secret', help='Twitter consumer secret')
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-u', '--user', type=str, metavar='user', dest='screen', help='User timeline to search')
@@ -25,11 +28,12 @@ def main():
     parser.add_argument('--lite', action='store_true', help='Output minimal information about tweets')
     parser.add_argument('--count', type=int, default=100, help='Maximum number of tweets to return')
     parser.add_argument('--geocode', type=str, help='optional geocode parameter when searching')
+
     parser.add_argument('-o', '--output', type=str, help='output file (default is stdout)')
 
     arguments = parser.parse_args()
 
-    twitter = api.API(app='twittergeo', config_file=arguments.config)
+    twitter = API(app='twittergeo', config_file=arguments.config)
 
     kwargs = {k:v for k, v in vars(arguments).items() if k in ('q', 'screen', 'geocode') and v is not None}
 
